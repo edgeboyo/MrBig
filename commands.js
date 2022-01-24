b690af6881184bfc43b1668fdc6ebb5f75533e0a
edgeboyo <mkolybko@gmail.com>
2022-01-24 15:14:17 +0000

    Set up commands

--- commands.js
+++ commands.js
@@ -1,7 +1,26 @@
 function setUpCommands(client) {
-  client.application.commands.create({
-    name: "test",
-    description: "A test command",
+  const commands = [
+    { name: "ping", description: "Ping command" },
+    { name: "send", description: "Send an epic attach" },
+  ];
+
+  client.application.commands.set(commands);
+
+  client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    switch (commandName) {
+      case "ping":
+        await interaction.reply("Pong");
+        break;
+      case "send":
+        await interaction.reply(
+          "Link for attachment waiting for you in http://text.com"
+        );
+        break;
+    }
   });
 }
 
